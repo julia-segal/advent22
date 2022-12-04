f9ede58a1d113790bf2d0cb383eeec49678abab6
Julia Segal <julia.segal@thetrainline.com>
2022-12-04 05:58:35 +0000

    Day 5 Prep

---
+++ src/main/kotlin/day5/day5.kt
@@ -0,0 +1,20 @@
+package day5
+
+import utils.Input
+import utils.Input.Day
+
+class Day5 {
+
+    fun part2(lines: List<String>) {
+    }
+
+    fun part1(lines: List<String>) {
+    }
+}
+
+fun main() {
+    val input = Input(Day.DAY5).getLines()
+
+    println("Part 1: ${Day5().part1(input)}")
+    println("Part 2: ${Day5().part2(input)}")
+}
