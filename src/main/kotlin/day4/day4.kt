0b3fb92ef20fd3e4b9555dd8cd9423297a060522
Julia Segal <julia.segal@thetrainline.com>
2022-12-04 05:55:57 +0000

    Day 4

--- src/main/kotlin/day4/day4.kt
+++ src/main/kotlin/day4/day4.kt
@@ -5,10 +5,36 @@ import utils.Input.Day
 
 class Day4 {
 
-    fun part2(lines: List<String>) {
+    fun part2(lines: List<String>):Int {
+        var count = 0
+        lines.map { line ->
+            val ranges = line.split(',')
+            val r1 = ranges[0].split('-')
+            val r2 = ranges[1].split('-')
+            when{
+                r1[0].toInt() in r2[0].toInt()..r2[1].toInt() -> count++
+                r1[1].toInt() in r2[0].toInt()..r2[1].toInt() -> count++
+                r2[0].toInt() in r1[0].toInt()..r1[1].toInt() -> count++
+                r2[1].toInt() in r1[0].toInt()..r1[1].toInt() -> count++
+                else -> {}
+            }
+        }
+        return count
     }
 
-    fun part1(lines: List<String>) {
+    fun part1(lines: List<String>):Int {
+        var count = 0
+        lines.map { line ->
+            val ranges = line.split(',')
+            val r1 = ranges[0].split('-')
+            val r2 = ranges[1].split('-')
+            when {
+                r1[0].toInt() <= r2[0].toInt() && r1[1].toInt() >= r2[1].toInt() -> count++
+                r1[0].toInt() >= r2[0].toInt() && r1[1].toInt() <= r2[1].toInt() -> count++
+                else -> {}
+            }
+        }
+        return count
     }
 }
 
--- src/test/kotlin/day4/Day4Test.kt
+++ src/test/kotlin/day4/Day4Test.kt
@@ -9,13 +9,18 @@ internal class Day4Test {
 
     private val input: List<String>
         get() {
-            val input = ""
+            val input = "2-4,6-8\n" +
+                    "2-3,4-5\n" +
+                    "5-7,7-9\n" +
+                    "2-8,3-7\n" +
+                    "6-6,4-6\n" +
+                    "2-6,4-8"
             return input.split('\n')
         }
 
     @Test
     fun part1() {
-        val expected = 0
+        val expected = 2
 
         val actual = sut.part1(input)
 
@@ -24,7 +29,7 @@ internal class Day4Test {
 
     @Test
     fun part2() {
-        val expected = 0
+        val expected = 4
 
         val actual = sut.part2(input)
 
