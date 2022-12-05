e7dd2fc5197f994e6794b4fc7dbd90d1a302517f
Julia Segal <julia.segal@thetrainline.com>
2022-12-05 07:10:08 +0000

    Day5 Part 2

--- src/main/kotlin/day5/day5.kt
+++ src/main/kotlin/day5/day5.kt
@@ -4,11 +4,72 @@ import utils.Input
 import utils.Input.Day
 
 class Day5 {
+    data class Instruction(val count: Int, val from: Int, val to: Int)
 
-    fun part2(lines: List<String>) {
+    private fun String.toInstruction(): Instruction {
+        val command = split(' ')
+        return Instruction(count = command[1].toInt(), from = command[3].toInt(), to = command[5].toInt())
     }
 
-    fun part1(lines: List<String>) {
+    private fun String.toStacks(stacks: HashMap<Int, MutableList<Char>>) {
+        var stackNumber = 1
+        var lineIndex = 1
+        while (lineIndex < this.length) {
+            val item = this[lineIndex]
+            if (!item.isWhitespace()) {
+                stacks[stackNumber]?.add(this[lineIndex])
+                    ?: run { stacks.put(stackNumber, mutableListOf(this[lineIndex])) }
+            }
+            lineIndex += 4
+            stackNumber++
+        }
+    }
+
+    private fun parse(lines: List<String>): Pair<HashMap<Int, MutableList<Char>>, List<Instruction>> {
+        val stacks = hashMapOf<Int, MutableList<Char>>()
+        val instructions = mutableListOf<Instruction>()
+
+        lines.map {
+            when {
+                it.startsWith("move") -> instructions.add(it.toInstruction())
+                it.startsWith(" 1 ") -> {}
+                it.startsWith("\n") -> {}
+                else -> it.toStacks(stacks)
+            }
+        }
+        stacks.forEach { (_, stackOfChars) -> stackOfChars.reverse() }
+        return Pair(stacks, instructions)
+    }
+
+    private fun moveIt(stacks: HashMap<Int, MutableList<Char>>, instructions: List<Instruction>): String {
+        instructions.map { instruction ->
+            repeat(instruction.count) {
+                stacks[instruction.to]?.add(stacks[instruction.from]!!.last())
+                stacks[instruction.from]?.removeLast()
+            }
+        }
+        return stacks.keys.map { stacks[it]?.last() }.joinToString("")
+    }
+
+    private fun moveIt2(stacks: HashMap<Int, MutableList<Char>>, instructions: List<Instruction>): String {
+        instructions.map { instruction ->
+            for (i in instruction.count - 1 downTo 0) {
+                val fromIndex = stacks[instruction.from]!!.lastIndex - i
+                stacks[instruction.to]?.add(stacks[instruction.from]!![fromIndex])
+                stacks[instruction.from]?.removeAt(fromIndex)
+            }
+        }
+        return stacks.keys.map { stacks[it]?.last() }.joinToString("")
+    }
+
+    fun part2(lines: List<String>): String {
+        val (stacks, instructions) = parse(lines)
+        return moveIt2(stacks, instructions)
+    }
+
+    fun part1(lines: List<String>): String {
+        val (stacks, instructions) = parse(lines)
+        return moveIt(stacks, instructions)
     }
 }
 
--- src/test/kotlin/day5/Day5Test.kt
+++ src/test/kotlin/day5/Day5Test.kt
@@ -3,19 +3,27 @@ package day5
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class DayNTest {
+internal class Day5Test {
 
     private val sut = Day5()
 
     private val input: List<String>
         get() {
-            val input = ""
+            val input = "    [D]    \n" +
+                    "[N] [C]    \n" +
+                    "[Z] [M] [P]\n" +
+                    " 1   2   3 \n" +
+                    "\n" +
+                    "move 1 from 2 to 1\n" +
+                    "move 3 from 1 to 3\n" +
+                    "move 2 from 2 to 1\n" +
+                    "move 1 from 1 to 2"
             return input.split('\n')
         }
 
     @Test
     fun part1() {
-        val expected = 0
+        val expected = "CMZ"
 
         val actual = sut.part1(input)
 
@@ -24,7 +32,7 @@ internal class DayNTest {
 
     @Test
     fun part2() {
-        val expected = 0
+        val expected = "MCD"
 
         val actual = sut.part2(input)
 
