a283fd5040aacfe12f22ac12d008327de90ad64b
Julia Segal <julia.segal@thetrainline.com>
2022-12-06 05:55:02 +0000

    Day7 prep

---
+++ src/main/kotlin/day7/day7.kt
@@ -0,0 +1,20 @@
+package day7
+
+import utils.Input
+import utils.Input.Day
+
+class Day7 {
+
+    fun part2(lines: List<String>) {
+    }
+
+    fun part1(lines: List<String>) {
+    }
+}
+
+fun main() {
+    val input = Input(Day.DAY7).getLines()
+
+    println("Part 1: ${Day7().part1(input)}")
+    println("Part 2: ${Day7().part2(input)}")
+}
---
+++ src/test/kotlin/day7/Day7Test.kt
@@ -0,0 +1,33 @@
+package day7
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day7Test {
+
+    private val sut = Day7()
+
+    private val input: List<String>
+        get() {
+            val input = ""
+            return input.split('\n')
+        }
+
+    @Test
+    fun part1() {
+        val expected = 0
+
+        val actual = sut.part1(input)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun part2() {
+        val expected = 0
+
+        val actual = sut.part2(input)
+
+        assertEquals(expected, actual)
+    }
+}
