5eeb5bb3c6be75028e73366298376a6beefbf141
Julia Segal <julia.segal@thetrainline.com>
2022-12-07 09:55:06 +0000

    Day 7 Refactor

--- src/main/kotlin/day7/day7.kt
+++ src/main/kotlin/day7/day7.kt
@@ -16,111 +16,113 @@ class Day7 {
         var size: Int,
         var children: MutableList<Node>,
         var parent: Node?
-    )
-
-    private fun Node.hasChild(name: String): Boolean {
-        children.map { if (it.name == name) { return true } }
-        return false
+    ) {
+        override fun toString(): String =
+            if (isDir) "$name $size $children ${parent?.name ?: ""}"
+            else "$name $size ${parent?.name ?: ""}"
     }
 
-    fun Node.child(name: String): Node {
-        children.map { if (it.name == name) { return it } }
+    private fun Node.child(name: String): Node {
+        children.map {
+            if (it.name == name) {
+                return it
+            }
+        }
         return this
     }
 
-    fun Node.add(node: Node) {
+    private fun Node.add(node: Node) {
         children.add(node)
     }
 
-    fun Node.totalSizes(): Int {
-        size = children.map { child ->
+    private fun Node.totalSizes(): Int {
+        size = children.sumOf { child ->
             if (child.isDir) {
                 child.totalSizes()
             } else {
                 child.size
             }
-        }.sum()
+        }
         return size
     }
 
     // Sort by size
-    fun Node.sorted(map: MutableMap<String, Int>) {
+    private fun Node.toSortedList(map: MutableMap<String, Int>) {
         children.map { child ->
             if (child.isDir) {
-                child.sorted(map)
+                child.toSortedList(map)
             }
         }
         map[this.name] = this.size
     }
 
-    fun Node.atMost(max: Int): Int {
+    private fun Node.atMost(max: Int): Int {
         var total = 0
         children.map { child ->
             if (child.isDir) {
-                if (child.size <= max) {
-                    total += (child.size + child.atMost(max))
+                total += if (child.size <= max) {
+                    (child.size + child.atMost(max))
                 } else {
-                    total += child.atMost(max)
+                    child.atMost(max)
                 }
             }
         }
         return total
     }
 
-    val String.isCommand: Boolean
+    private val String.isCommand: Boolean
         get() = this == "$"
 
-    val String.isDir: Boolean
+    private val String.isDir: Boolean
         get() = this.startsWith("dir")
 
-    val String.isFile: Boolean
+    private val String.isFile: Boolean
         get() = this.first().isDigit()
 
-    val String.listing: Boolean
-        get() = this[0].isDigit()
-
-    fun List<String>.toCommand(): Command =
+    private fun List<String>.toCommand(): Command =
         if (this[1] == "cd") Command(this[1], this[2])
         else Command(this[1], null)
 
-    fun parse(lines: List<String>): Node {
+    private fun parse(lines: List<String>): Node {
         val root = Node("/", true, 0, mutableListOf(), null)
         var currentNode: Node = root
-        var curIndex = 0
-        while (curIndex < lines.lastIndex) {
-            val line = lines[curIndex].split(' ')
+        var index = 0
+        while (index < lines.lastIndex) {
+            val line = lines[index].split(' ')
             if (line.first().isCommand) {
                 val command = line.toCommand()
                 when (command.command) {
                     "cd" -> {
-                        val arg = command.arg!!
-                        when (arg) {
-                            "/" -> currentNode = root
-                            ".." -> currentNode = currentNode.parent!!
-                            else -> {
-                                if (currentNode.hasChild(command.arg)) {
-                                    currentNode = currentNode.child(command.arg)
-                                }
-                            }
+                        currentNode = when (command.arg!!) {
+                            "/" -> root
+                            ".." -> currentNode.parent!!
+                            else -> currentNode.child(command.arg)
                         }
-                        curIndex++
+                        index++
                     }
 
                     "ls" -> {
-                        while (curIndex < lines.lastIndex) {
-                            curIndex++
-                            val l = lines[curIndex].split(' ')
-                            if (l.first().isDir) {
-                                if (currentNode?.hasChild(l.last()) == false) {
-                                    val newNode = Node(l.last(), true, 0, mutableListOf<Node>(), currentNode)
-                                    currentNode.add(newNode)
-                                }
-                            } else if (l.first().isFile) {
-                                if (currentNode?.hasChild(l.last()) == false) {
-                                    val newNode =
-                                        Node(l.last(), false, l.first().toInt(), mutableListOf<Node>(), currentNode)
-                                    currentNode.add(newNode)
-                                }
+                        while (index < lines.lastIndex) {
+                            index++
+                            val file = lines[index].split(' ')
+                            if (file.first().isDir) {
+                                currentNode.add(
+                                    Node(file.last(),
+                                        true,
+                                        0,
+                                        mutableListOf(),
+                                        currentNode)
+                                )
+                            } else if (file.first().isFile) {
+                                currentNode.add(
+                                    Node(
+                                        file.last(),
+                                        false,
+                                        file.first().toInt(),
+                                        mutableListOf(),
+                                        currentNode
+                                    )
+                                )
                             } else {
                                 break
                             }
@@ -135,13 +137,13 @@ class Day7 {
     fun part2(lines: List<String>): Int {
         val FREE_NEEDED = 30000000
         val MAX = 70000000
+
         val tree = parse(lines)
         val unused = MAX - tree.totalSizes()
         val need = FREE_NEEDED - unused
         val sortedMap = mutableMapOf<String, Int>()
-        tree.sorted(sortedMap)
-        val values = sortedMap.values.sorted()
-        values.map { value ->
+        tree.toSortedList(sortedMap)
+        sortedMap.values.map { value ->
             if (value > need) {
                 return value
             }
