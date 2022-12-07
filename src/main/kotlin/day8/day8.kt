d7e5f4d53275b8c1c5c5244288a58d45419bde9f
Julia Segal <julia.segal@thetrainline.com>
2022-12-07 10:01:04 +0000

    Day 8 Prep

---
+++ src/main/kotlin/day8/day8.kt
@@ -0,0 +1,20 @@
+package day8
+
+import utils.Input
+import utils.Input.Day
+
+class Day8 {
+
+    fun part2(lines: List<String>) {
+    }
+
+    fun part1(lines: List<String>) {
+    }
+}
+
+fun main() {
+    val input = Input(Day.DAY1).getLines()
+
+    println("Part 1: ${Day8().part1(input)}")
+    println("Part 2: ${Day8().part2(input)}")
+}
